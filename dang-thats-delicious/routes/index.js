080e8edcecd4d35635705437c4cd8d6d32adeef8
Robert <robert@Roberts-MacBook-Pro.local>
2019-04-19 10:30:57 -0600

    Stores Search on page load

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -6,7 +6,8 @@ const storeController = require('../controllers/storeController');
 // Do work here
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
 

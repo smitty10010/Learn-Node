53d45671c0e2471b73731d44bc2ef8a27cd935ff
Robert <robert@Roberts-MacBook-Pro.local>
2019-05-03 11:53:42 -0600

    added logout and avatar

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -6,3 +6,18 @@ exports.login = passport.authenticate('local', {
         successRedirect: '/',
         successFlash: 'You have logged in!',
 });
+
+exports.logout = (req, res) => {
+        req.logout();
+        req.flash('Success', 'You have logged out!');
+        res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+        if (req.isAuthenticated()) {
+                next();
+                return;
+        }
+        req.flash('error', 'Oops you must be logged in!');
+        res.redirect('/login');
+};
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -10,7 +10,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 router.post(
         '/add',
         storeController.upload,
@@ -28,7 +28,9 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 router.post('/register', userController.validateRegister, catchErrors(userController.register), authController.login);
+router.get('/logout', authController.logout);
 
 module.exports = router;

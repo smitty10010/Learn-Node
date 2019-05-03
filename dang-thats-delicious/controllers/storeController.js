78df453d8ad9e74e28499eb8c02fc427d3a56490
Robert <robert@Roberts-MacBook-Pro.local>
2019-05-03 14:57:44 -0600

    search function

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -95,3 +95,20 @@ exports.getStoresByTag = async (req, res) => {
 
         res.render('tag', { tags, title: 'Tags', tag, stores });
 };
+
+exports.searchStores = async (req, res) => {
+        const stores = await Store.find(
+                {
+                        $text: {
+                                $search: req.query.q,
+                        },
+                },
+                {
+                        score: { $meta: 'textScore' },
+                }
+        )
+                .sort({
+                        score: { $meta: 'textScore' },
+                })
+                .limit(5);
+};

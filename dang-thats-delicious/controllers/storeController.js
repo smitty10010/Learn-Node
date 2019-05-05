3261d7716710df9692ba0b71a5d2bcbc5648e599
Robert <robert@Roberts-MacBook-Pro.local>
2019-05-04 22:25:20 -0600

    added map page with search

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -128,7 +128,11 @@ exports.mapStores = async (req, res) => {
                 },
         };
         const stores = await Store.find(query)
-                .select('slug name description location')
+                .select('slug name description location photo')
                 .limit(10);
         res.json(stores);
+};
+
+exports.mapPage = (req, res) => {
+        res.render('map', { title: 'Map' });
 };

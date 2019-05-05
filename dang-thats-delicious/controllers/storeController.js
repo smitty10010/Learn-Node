19564a2d9e51b8ce8c5b0c6c2b8f82ed190ffcff
Robert <robert@Roberts-MacBook-Pro.local>
2019-05-04 21:25:57 -0600

    created geo API

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -113,3 +113,22 @@ exports.searchStores = async (req, res) => {
                 .limit(5);
         res.json(stores);
 };
+
+exports.mapStores = async (req, res) => {
+        const coordinates = [req.query.lng, req.query.lat].map(parseFloat);
+        const query = {
+                location: {
+                        $near: {
+                                $geometry: {
+                                        type: 'Point',
+                                        coordinates,
+                                },
+                                $maxDistance: 10000, // 10km
+                        },
+                },
+        };
+        const stores = await Store.find(query)
+                .select('slug name description location')
+                .limit(10);
+        res.json(stores);
+};

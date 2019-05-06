e803364e56c78671732e62d305b4d1756472e3c3
Robert <robert@Roberts-MacBook-Pro.local>
2019-05-06 10:13:35 -0600

    added top page and review icon

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -41,7 +41,7 @@ router.post('/account/reset/:token', authController.confirmedPasswords, catchErr
 router.get('/map', storeController.mapPage);
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
-
+router.get('/top', catchErrors(storeController.getTopStores));
 // API routes
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
